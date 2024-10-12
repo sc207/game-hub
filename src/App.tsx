780031702600859fd168b2999effecd607311855
sc207 <chauhansunny0207@gmail.com>
2024-10-12 03:56:16 -0400

    Fix the issue with chakra menus

--- src/App.tsx
+++ src/App.tsx
@@ -48,15 +48,18 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={2} marginBottom={5}>
-          <PlatformSelector
+        <Flex paddingLeft={2} marginBottom={5}>
+         <Box marginRight={5}>
+         <PlatformSelector
             selectedPlatform={gameQuery.platform}
             onSelectPlatform={(platform) =>
               setGameQuery({ ...gameQuery, platform })
             }
           />
+         </Box>
+          
           <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=>setGameQuery({...gameQuery,sortOrder})}/>
-        </HStack>
+        </Flex>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
